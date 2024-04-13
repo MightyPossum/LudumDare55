7f60542089c8ae5c918d9b5d6b4608c6b8338832
Sindre Joa Sømme <sindresomme@gmail.com>
2024-04-13 22:58:01 +0200

    setup new level

--- Script/menu scripts/main_menu.gd
+++ Script/menu scripts/main_menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	get_tree().paused = false
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scene/Level.tscn")
+	get_tree().change_scene_to_file("res://Scene/new_level.tscn")
 
 
 func _on_exit_pressed():
