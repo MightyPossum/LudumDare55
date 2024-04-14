72abc64dea4e84a173bb4d8d2ce5227270b26f40
Sindre Joa Sømme <sindresomme@gmail.com>
2024-04-14 22:31:26 +0200

    Fixed Sacred Circle

--- Script/SacredObject.gd
+++ Script/SacredObject.gd
@@ -11,6 +11,6 @@ func _process(delta):
 	pass
 
 func _on_area_area_entered(area):
-	if area.is_in_group("enemy")
+	if area.is_in_group("enemy"):
 		area.get_parent()._attacked_sacred_object();
-		get_node("/root/Map1")._handle_life_lost()
+		get_node("/root/Map1")._handle_life_lost
