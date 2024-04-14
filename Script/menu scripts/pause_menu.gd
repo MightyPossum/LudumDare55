26f1b41de31c4e72453548703fdfb649ae338d24
Sindre Joa Sømme <sindresomme@gmail.com>
2024-04-14 14:58:15 +0200

    Added Wave Timer

--- Script/menu scripts/pause_menu.gd
+++ Script/menu scripts/pause_menu.gd
@@ -6,5 +6,6 @@ func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Scene/menues/main_menu.tscn")
 
 func _on_resume_pressed():
-	player.capture_mouse()
-	player.pauseMenu() == false
+	print('FIUCK')
+	%Player.capture_mouse()
+	get_node("/root/Map1").pauseMenu()
