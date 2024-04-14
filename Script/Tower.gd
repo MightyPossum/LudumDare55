ff2b657ea7e2d3d6d2d921d0facef07385d22838
EyeLax <43846424+EyeLax@users.noreply.github.com>
2024-04-14 12:40:33 +0200

    Tower Defense

--- Script/Tower.gd
+++ Script/Tower.gd
@@ -1,22 +1,39 @@
 extends Node3D
 
-var enemy_array = []
-var built = false
+var enemies_in_range:Array[Node3D]
+var current_enemy:Node3D = null
+
+@export var projectileModel : PackedScene
+
+var last_fire_time:int
+@export var fire_rate:int = 1000
 
 func _ready():
-	if built:
-		self.get_node("StaticBody3D/Turret").get_shape().radius = 0.5
+	pass
 
-func _physics_process(delta):
-	turn()
+func _process(delta):
+	if current_enemy != null:
+		_attack(current_enemy, delta)
 	
-func turn():
-	var enemy_postion = global_position
-	get_node("StaticBody3D/Turret").look_at(enemy_postion)
-
+func _attack(rtarget, delta):
+	if Time.get_ticks_msec() > (last_fire_time+fire_rate):
+		var projectile = projectileModel.instantiate()
+		get_parent().add_child(projectile)
+		
+		var target_direction = rtarget.global_transform.origin
+		
+		projectile.global_position = $Turret/Muzzle.global_position
+		projectile.look_at(target_direction)
+		
+		last_fire_time = Time.get_ticks_msec()
+		
 func _on_area_3d_area_entered(area):
-	enemy_array.append(area.get_parent())
-	print(enemy_array)
+	if current_enemy == null:
+		current_enemy = area
+		
+	enemies_in_range.append(area)
+	print(enemies_in_range.size())
 
 func _on_area_3d_area_exited(area):
-	enemy_array.erase(area.get_parent())
+	enemies_in_range.erase(area)
+	print(enemies_in_range.size())
