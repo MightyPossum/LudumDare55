0392ceb5b9c5617233c1f1dba58cde1811d27aa4
Sindre Joa Sømme <sindresomme@gmail.com>
2024-04-21 10:31:27 +0200

    Typo fixes

--- Script/save_game_system/StartMenuSaveHandling.gd
+++ Script/save_game_system/StartMenuSaveHandling.gd
@@ -8,32 +8,32 @@ var reset_in_progress: bool = false
 
 ## Saves to the actual file
 func save_to_file() -> void:
-    ResourceSaver.save(game_data, USER_DATA_PATH + SAVE_FILE_NAME)
+	ResourceSaver.save(save_game_data, USER_DATA_PATH + SAVE_FILE_NAME)
 
 func _ready() -> void:
-    verify_save_directory(USER_DATA_PATH)
+	verify_save_directory(USER_DATA_PATH)
 
 ## verify that the save directory is available
 func verify_save_directory(path : String) -> void:
-    DirAccess.make_dir_absolute(path)
+	DirAccess.make_dir_absolute(path)
 
 func clear_save_game() -> void:
 
-    if ResourceLoader.exists(USER_DATA_PATH + SAVE_FILE_NAME):
+	if ResourceLoader.exists(USER_DATA_PATH + SAVE_FILE_NAME):
 
-        GLOBALVARIABLES.current_wave = 1
-        GLOBALVARIABLES.amount_of_cash = 0
-        GLOBALVARIABLES.health = 10
-        GLOBALVARIABLES.tower_cost = 500
-        GLOBALVARIABLES.scoreboard_array = [Array([0,0]),Array([0,0]),Array([0,0]),Array([0,0]),Array([0,0])]
+		GLOBALVARIABLES.current_wave = 1
+		GLOBALVARIABLES.amount_of_cash = 0
+		GLOBALVARIABLES.health = 10
+		GLOBALVARIABLES.tower_cost = 500
+		GLOBALVARIABLES.scoreboard_array = [Array([0,0]),Array([0,0]),Array([0,0]),Array([0,0]),Array([0,0])]
 
-        var wave_state_details = Array([GLOBALVARIABLES.current_wave, GLOBALVARIABLES.amount_of_cash])
+		var wave_state_details = Array([GLOBALVARIABLES.current_wave, GLOBALVARIABLES.amount_of_cash])
 		save_game_data.save_array(GLOBALVARIABLES.WAVE_STATE_DETAILS, wave_state_details)
-        var scoreboard = Array([GLOBALVARIABLES.scoreboard_array])
+		var scoreboard = Array([GLOBALVARIABLES.scoreboard_array])
 		save_game_data.save_array(GLOBALVARIABLES.SCOREBOARD, scoreboard)
 
-        ## SAVING BUILD SITES
-        var build_sites_built: Array = Array()
-        game_data.save_array(GLOBALVARIABLES.BUILD_SITES, Array([build_sites_built]))
+		## SAVING BUILD SITES
+		var build_sites_built: Array = Array()
+		save_game_data.save_array(GLOBALVARIABLES.BUILD_SITES, Array([build_sites_built]))
 
-        save_to_file()
+		save_to_file()
